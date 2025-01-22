8ee52509c664d1dc4aff4e4e34538a77c9600036
tervingo <keltivas@gmail.com>
2025-01-22 17:17:06 +0100

    More cosmetic changes

--- src/WindDirectionIndicator.js
+++ src/WindDirectionIndicator.js
@@ -9,35 +9,38 @@ const WindDirectionIndicator = ({ direction, speed, rose }) => {
 
   const x = radius + radius * Math.sin(angleInRadians) - triangleSize / 2;
   const y = radius - radius * Math.cos(angleInRadians) - triangleSize / 2;
+  const circleRadius = 10; // Radius of the inner circle
 
   return (
     <div className='wind-direction-container'>
-        <div className="wind-direction-indicator">
+      <div className="wind-direction-indicator">
         <div className="circle">
-            <div className="label north">N</div>
-            <div className="label east">E</div>
-            <div className="label south">S</div>
-            <div className="label west">W</div>
-            <div
-            className="triangle"
+          <div className="label north">N</div>
+          <div className="label east">E</div>
+          <div className="label south">S</div>
+          <div className="label west">W</div>
+          <div 
+            className="circle-indicator" 
             style={{
-                top: `${y}px`,
-                left: `${x}px`,
-            }}
-            ></div>
-            <div className="center-labels">
-              <Typography variant="h5">
-                {speed} km/h
-              </Typography>
-              <Typography variant="h5">
+              top: `${y}px`,
+              left: `${x}px`,
+              width: `${2 * circleRadius}px`, // Set width and height for circle
+              height: `${2 * circleRadius}px`,
+            }} 
+          />
+          <div className="center-labels">
+            <Typography variant="h5">
+              {speed} km/h
+            </Typography>
+            <Typography variant="h5">
               {direction}°
-              </Typography>
-              <Typography variant="h5">
+            </Typography>
+            <Typography variant="h5">
               {rose}
-              </Typography>            
-            </div>
+            </Typography>           
+          </div>
         </div>
-        </div>
+      </div>
     </div>
   );
 };
